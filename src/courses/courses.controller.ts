c065665293ee66fdf096ee05ec1c9b3561202746
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-02-24 22:36:17 +0100

    manuak status code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
@@ -13,6 +21,7 @@ export class CoursesController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
     return body;
   }
