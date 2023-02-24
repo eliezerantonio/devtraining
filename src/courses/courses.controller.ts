9a04ddc8698d64c0c7c1d2fc301d028865b3668d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-02-24 21:39:21 +0100

    working with controller

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+  @Get('')
+  findAll() {
+    return 'Listagem de cursos';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+      
+      
+    return `Curso #${id}`;
+  }
+}
