45192006b52655be7d7074b28936700620d734b6
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-02-27 13:12:05 +0100

    workin with delete and path method

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 
@@ -24,5 +26,15 @@ export class CoursesController {
   @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
     return body;
+  }
+
+  @Patch('id')
+  update(@Param('id') id: string, @Body() body) {
+    return ` Atualização do curso ${id}`;
+  }
+
+  @Delete('id')
+  delete(@Param('id') id: string) {
+    return ` Exclusão do curso ${id}`;
   }
 }
