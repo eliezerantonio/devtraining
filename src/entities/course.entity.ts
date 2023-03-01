57792e90b069e3fee545dce9d90f7f10cf5185b3
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-01 08:10:14 +0100

    first entity with typeorm

--- src/entities/course.entity.ts
+++ src/entities/course.entity.ts
@@ -1,6 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('courses')
 export class Course {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @Column('json', { nullable: true })
   tags: string[];
 }
