42ded82da774f185a729f47b9ccf2c32ce67681a
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-01 13:27:57 +0100

    feat:add TagEntity on CourseModule

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CourseEntity } from 'src/courses/entities/course.entity/course.entity';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { TagEntity } from './entities/tag.entity/tag.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CourseEntity])],
+  imports: [TypeOrmModule.forFeature([CourseEntity, TagEntity])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
