1a1647561976b0879418862fd08ce94d9bedfe80
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-01 13:21:05 +0100

    Move CourseEntity to folder entities

---
+++ src/courses/entities/course.entity/course.entity.ts
@@ -0,0 +1,25 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { TagEntity } from '../tag.entity/tag.entity';
+
+@Entity('courses')
+export class CourseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @JoinTable()
+  @ManyToMany(() => TagEntity, (tag) => tag.courses)
+  tags: string[];
+}
