cd522594d958b40979b4914d2c393b7f24f3323d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-01 22:34:22 +0100

    create first migration

---
+++ src/migrations/1677706115665-CourseRefactoring.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CourseRefactoring1677706115665 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "course" RENAME COLUMN "name" TO "course`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "course" RENAME COLUMN "course" TO "name`,
+    );
+  }
+}
