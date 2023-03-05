e72fcc6c151220aeb5b5f510657d9c1753b51bfd
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-05 16:10:13 +0100

    refactor:improve TantEntity

--- src/courses/entities/tag.entity/tag.entity.ts
+++ src/courses/entities/tag.entity/tag.entity.ts
@@ -1,14 +1,34 @@
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { CourseEntity } from '../course.entity/course.entity';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
 
 @Entity('tags')
 export class TagEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   name: string;
 
   @ManyToMany(() => CourseEntity, (course) => course.tags)
   courses: CourseEntity[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @BeforeInsert()
+  generatedId() {
+    if (this.id) {
+      return;
+    }
+
+    this.id = uuidv4();
+  }
 }
