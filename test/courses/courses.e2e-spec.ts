c9173e284e5681f1a3965d2dd1305d1a9a38c77b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-10 01:28:10 +0100

    test: e2e test implement POST method

--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -1,20 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { CoursesModule } from '../../src/courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateCourseDto } from '../../src/courses/dto/create-course.dto/create-course.dto';
 
 describe('Courses: /courses', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  const course: CreateCourseDto = {
+    name: 'Nestjs com TypeORM',
+    description: 'Api rest cpm nestjs',
+    tags: ['nestjs', 'typeorm', 'nodejs'],
+  };
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoursesModule],
+      imports: [
+        CoursesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          port: 5433,
+          host: 'localhost',
+          username: 'postgres',
+          password: 'docker',
+          database: 'testdb',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
+    
+    
+    afterAll(async () => {
+        
+        await app.close();
+    })
 
- 
-  it.todo('Create POST  /courses');
+  it('Create POST  /courses', () => {
+    return request(app.getHttpServer()).post('/courses').send(course).expect(HttpStatus.CREATED);
+  });
 });
